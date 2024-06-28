25cf6c0cd694481d6d6140302691ce1015162222
Wojtaszek104 <woy104@tlen.pl>
2024-06-28 17:01:45 +0200

    feat: working version

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,15 @@
-export { Header } from "./header";
+import UpcommingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcommingEventsSkeleton from "./skeleton/upcoming-events"
+import AccordionHeaderSkeleton from "./skeleton/accordion-header"
+import ProjectCardSkeleton from "./skeleton/project-card"
+import KanbanSkeleton from "./skeleton/kanban"
+import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivites from "./home/latest-activities";
+
+export {
+    UpcommingEvents, DealsChart,
+    UpcommingEventsSkeleton, AccordionHeaderSkeleton,
+    ProjectCardSkeleton, KanbanSkeleton, DashboardTotalCountCard,
+    LatestActivites
+};
